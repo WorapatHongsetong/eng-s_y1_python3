e743f762e8d710c27a21b9b925f9276bb4ccf8e7
QuantumTacoNinja <worapat1h@gmail.com>
2025-01-15 10:41:19 +0700

    Now server is alive, even client disconnected.

--- day3(servers thingies again)/server.py
+++ day3(servers thingies again)/server.py
@@ -1,65 +1,63 @@
 # server side
-
 import socket
-import sys
-from pyexpat.errors import messages
 
 HOST = '0.0.0.0'
 PORT = 21002
-s = None
 
 kill_server = False
 
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("Socket created")
-except OSError as msg:
-    s = None
-    print(f"Error creating socket: {msg}")
-    exit(1)
-
-try:
     s.bind((HOST, PORT))
     s.listen()
-    print("Socket bound and listening")
+    print("Server is running and listening on port", PORT)
 except OSError as msg:
-    print("Error binding/listening!")
-    s.close()
+    print("Failed to set up server:", msg)
     exit(1)
 
-conn, addr = s.accept()
-with conn:
-    print('Connection accepted from ', addr)
+while not kill_server:
+    print("Waiting for a new client to connect...")
+    conn, addr = s.accept()
+    print("Connection accepted from", addr)
 
-    while True:
-        message_received = ""
+    with conn:
         while True:
-            data = conn.recv(32)
-            if data:
-                print('Received data chunk from client: ', repr(data))
-                message_received += data.decode()
-                if message_received.endswith("\n"):
+            try:
+                message_received = ""
+                while True:
+                    data = conn.recv(32)
+                    if data:
+                        print("Received data chunk from client:", repr(data))
+                        message_received += data.decode()
+                        if message_received.endswith("\n"):
+                            break
+                    else:
+                        print("Connection lost!")
+                        break
+
+                if message_received:
+                    if message_received.strip() == "exit()":
+                        print(addr, "disconnected")
+                        break
+
+                    print("Received message:", message_received.strip())
+                    
+                    reply = input("Reply to client: ")
+                    if reply.strip() == "kill()":
+                        kill_server = True
+                        print("Shutting down server...")
+                        break
+                    elif reply:
+                        conn.send(("Server: " + reply + "\n").encode())
+                else:
                     break
-            else:
-                print("Connection lost!")
+
+            except ConnectionResetError:
+                print("Client forcibly closed the connection.")
                 break
 
-        if message_received:
-            if message_received.endswith("exit()\n"):
-                print(addr, "disconnected")
-                break
-
-            print("Received message: ", message_received)
-            # conn.send(("Server summarized: " + message_received[:10] + "\n").encode())
-
-            reply = input("Reply to client: ")
-            if reply == "kill()":
-                kill_server = True
-            elif reply:
-                conn.send(("Server: " + reply + "\n").encode())
-
-        else:
-            break
+    conn.close()
+    print("Connection with", addr, "closed.")
 
 s.close()
 print("Server finished")
