c5c35464b44986f019c515c56e686906b6ba5dc5
QuantumTacoNinja <worapat1h@gmail.com>
2025-01-20 13:44:47 +0700

    Day6 Fix

--- day6/day6.py
+++ day6/day6.py
@@ -20,66 +20,60 @@ def euler_number_ps(start_term: int, end_term: int, que=None) -> Decimal:
 def worker(start_index, end_index):
     return euler_number_ps(start_index, end_index)
 
-if __name__ == "__main__":
+# multiprocessing.
+if 1:
     start_time = time.time()
 
-    N = 1000  # Number of terms per process
-    process_count = 8  # Number of processes
+    N = 1000  
+    process_count = 8  
 
     num_cores = multiprocessing.cpu_count()
     print(f"Number of cores: {num_cores}")
-
-    # Create the pool of workers
+    
     pool = multiprocessing.Pool(processes=process_count)
-
-    # Prepare ranges for the terms each process will handle
+    
     tasks = [(N * i, N * (i + 1)) for i in range(process_count)]
     
-    # Start the processes and collect results
     results = pool.starmap(worker, tasks)
 
-    # Close the pool and wait for all workers to finish
     pool.close()
     pool.join()
 
     end_time = time.time()
     print(f"Multiprocessing finished: {end_time - start_time} seconds")
 
-    # Combine the results from all processes
     result = sum(results)
 
     print(f"Result = {result}")
 
+# threading.
 if 0:
 
     start_time = time.time()
 
     qres = Queue()
 
-    N = 1000  # Number of terms per thread
-    thread_count = 8  # Number of threads
+    N = 1000  
+    thread_count = 8 
 
     num_cores = multiprocessing.cpu_count()
     print(f"Number of core: {num_cores}")
 
-    # Create and start threads
     thread_list = []
     for i in range(thread_count):
         start_index = N * i
-        end_index = N * (i + 1)  # This makes sure each thread handles a range of terms
+        end_index = N * (i + 1)  
         t = threading.Thread(target=euler_number_ps, args=(start_index, end_index, qres))
         thread_list.append(t)
         t.start()
         print(f"Thread {i} started processing terms {str(start_index).rjust(15)} to {str(end_index - 1).rjust(15)}.")
 
-    # Wait for all threads to complete
     for t in thread_list:
         t.join()
 
     end_time = time.time()
     print(f"Threading finished: {end_time - start_time} seconds")
 
-    # Combine the results from all threads
     result = Decimal(0)
     while not qres.empty():
         result += qres.get()
